0f01e13cce8f5624206e71eb290e99a36a3fe27d
Sergii Kholodylo <sergiikholodylo@Sergiis-MacBook-Pro.local>
2019-01-21 20:34:19 +0200

    The Lesson 3 is Over

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import avatar from 'theme/assets/lisa';
 
 import Styles from './styles.m.css';
 
+import homer from 'theme/assets/homer';
+
 export default class StatusBar extends Component {
+    static defaultProps = {
+        avatar:               homer,
+        currentUserFirstName: 'James',
+        currentUserLastName:  'Bond',
+    };
+
     render() {
+        const {
+            avatar,
+            currentUserFirstName,
+            currentUserLastName,
+        } = this.props;
+
         return (
             <section className = { Styles.statusBar } >
                 <button>
                     <img src = { avatar } />
-                    <span>Lisa</span>
+                    <span> { currentUserFirstName} </span>
                             &nbsp;
-                    <span>Simpson</span>
+                    <span> { currentUserLastName } </span>
                 </button>
             </section>
         );
     }
 }
 
+StatusBar.propTypes = {
+    avatar:               PropTypes.string.isRequired,
+    currentUserFirstName: PropTypes.string.isRequired,
+    currentUserLastName:  PropTypes.string.isRequired,
+};
